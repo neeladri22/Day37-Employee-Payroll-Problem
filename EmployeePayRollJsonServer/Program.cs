3214d5e553152d2202c8fe09a6877980a3ea1dbf
neeladri22 <neeladri22@gmail.com>
2022-07-31 11:29:14 +0530

    Welcome to Employee Payroll json server program

---
+++ EmployeePayRollJsonServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayRollJsonServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee pay roll json server program");
+        }
+    }
+}
