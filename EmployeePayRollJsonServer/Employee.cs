4bd68d8faa772a7a370f777ac5633e0df57a4bae
neeladri22 <neeladri22@gmail.com>
2022-07-31 15:06:19 +0530

    Retrieve all employee details

---
+++ EmployeePayRollJsonServer/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayRollJsonServer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace JsonServer
+    {
+        public class Employee
+        {
+
+            public int id { get; set; }
+            public string name { get; set; }
+            public string salary { get; set; }
+
+        }
+    }
+   
+}
